9b65229813a3e7319dc7249612a9dda1950083db
Amrta Kautsar Faruqi <amrta.faruqi@gmail.com>
2015-06-12 17:02:46 +0800

    Complete up and down move, Complete scoring system

--- app.js
+++ app.js
@@ -1,94 +1,115 @@
+
+// setting up utility function for setting ang getting data-value attribute
+
+jQuery.fn.extend({
+  getDataValue: function() {
+    return parseInt(this.attr("data-value"));
+  },
+  setDataValue: function(value) {
+    return this.attr("data-value", value);
+  }
+});
+
+$(window).on("resize", function() {
+  var width = $("[data-col]").width();
+  $("[data-col]").height(width);
+});
+
+
 $(function() {
 
-  // setting up utility function for setting ang getting data-value attribute
-  jQuery.fn.extend({
-    getDataValue: function() {
-      return parseInt(this.attr("data-value"));
-    },
-    setDataValue: function(value) {
-      return this.attr("data-value", value);
-    }
-  });
+  // init window resize to match column height & width value
+  $(window).resize();
 
-  var rowCount = $(".row").length;
+  // init score
 
   function getRandCel(num) {
+    setTimeout(function() {
+      var emptyCells = $("[data-value=0]"),
+          emptyCellsCount = emptyCells.length;
 
-    var emptyCells = $("[data-value=0]"),
-        emptyCellsCount = emptyCells.length;
+      // console.log(emptyCells, " : ", emptyCellsCount);
 
-    console.log(emptyCells, " : ", emptyCellsCount);
+      if (num == undefined) { num = 1; } // set default value if num is undefined
 
-    if (num == undefined) { num = 1; } // set default value if num is undefined
+      for(var i = 0; i < num; i++) {
 
-    for(var i = 0; i < num; i++) {
+        var randIndex = _.random(0, emptyCellsCount - 1),
+            selectedCell = emptyCells.eq(randIndex);
 
-      var randIndex = _.random(0, emptyCellsCount - 1),
-          selectedCell = emptyCells.eq(randIndex);
-
-      selectedCell.setDataValue(2);
-      selectedCell.text(2);
-    }
+        selectedCell.setDataValue(2);
+        selectedCell.text(2);
+      }
+    }, 500);
   }
 
   function initGrid() {
     getRandCel(2);
   }
 
+  function updateBoxScore(value) {
+    var boxScore = $("[data-score]"),
+        score = parseInt(boxScore.text());
+
+    boxScore.text(score + value);
+
+    return;
+  }
+
   function checkDirection(direction) {
     switch (direction) {
-      case 37:
-        // Left 
-
+      case 37: // Left 
+        
         var newRandVal = false;
 
         for(var k = 1; k <= 4; k++) {
 
           var currentRow = $("[data-row=" + k + "]");
 
-          for(var i = 1; i <= 4; i++) {
-            var current = currentRow.find("[data-col=" + i + "]");
+            for(var i = 1; i <= 4; i++) {
+              var current = currentRow.find("[data-col=" + i + "]");
 
-            for(var j = i + 1; j <= 4; j++) {
-              var next = currentRow.find("[data-col=" + j + "]"),
-                  nextValue = next.getDataValue(),
-                  currentValue = current.getDataValue();
+              for(var j = i + 1; j <= 4; j++) {
+                var next = currentRow.find("[data-col=" + j + "]"),
+                    nextValue = next.getDataValue(),
+                    currentValue = current.getDataValue();
 
 
-              if ((currentValue != 0) && (currentValue < nextValue)) {
-                break;
-              } else if ((currentValue == 0) && (nextValue > 0)) {
+                if ((currentValue != 0) && (currentValue < nextValue)) {
+                  break;
+                } else if ((currentValue == 0) && (nextValue > 0)) {
 
-                current.setDataValue(nextValue);
-                current.text(nextValue);
+                  current.setDataValue(nextValue);
+                  current.text(nextValue);
 
-                next.setDataValue(0);
-                next.text(0);
+                  next.setDataValue(0);
+                  next.text("");
 
-                newRandVal = true;
-              } else if ((currentValue == nextValue) && (currentValue > 0)) {
-                var addResult = currentValue + nextValue;
+                  newRandVal = true;
+                } else if ((currentValue == nextValue) && (currentValue > 0)) {
+                  var addResult = currentValue + nextValue;
 
-                current.setDataValue(addResult);
-                current.text(addResult);
+                  current.setDataValue(addResult);
+                  current.text(addResult);
 
-                next.setDataValue(0);
-                next.text(0);
+                  next.setDataValue(0);
+                  next.text("");
 
-                newRandVal = true;
-              }
+                  updateBoxScore(addResult);
 
-            } // end for loop 'j' 
+                  newRandVal = true;
+                }
 
-          } // end for loop 'i'
+              } // end for loop 'j' 
+
+            } // end for loop 'i'
 
         } // end for loop 'k'
 
         if (newRandVal) { getRandCel() }
 
         break;
-      case 39:
-        // Right
+      case 39: // Right
 
         var newRandVal = false;
         
@@ -96,11 +117,63 @@ $(function() {
 
           var currentRow = $("[data-row=" + k + "]");
 
-          for(var i = 4; i >= 1; i--) {
-            var current = currentRow.find("[data-col=" + i + "]");
+            for(var i = 4; i >= 1; i--) {
+              var current = currentRow.find("[data-col=" + i + "]");
 
-            for(var j = i - 1; j >= 1; j--) {
-              var next = currentRow.find("[data-col=" + j + "]"),
+              for(var j = i - 1; j >= 1; j--) {
+                var next = currentRow.find("[data-col=" + j + "]"),
+                    nextValue = next.getDataValue(),
+                    currentValue = current.getDataValue();
+
+                if ((currentValue != 0) && (currentValue < nextValue)) {
+                  break;
+                } else if ((currentValue == 0) && (nextValue > 0)) {
+
+                  current.setDataValue(nextValue);
+                  current.text(nextValue);
+
+                  next.setDataValue(0);
+                  next.text("");
+
+                  newRandVal = true;
+                } else if ((currentValue == nextValue) && (currentValue > 0)) {
+                  var addResult = currentValue + nextValue;
+
+                  current.setDataValue(addResult);
+                  current.text(addResult);
+
+                  next.setDataValue(0);
+                  next.text("");
+
+                  updateBoxScore(addResult);
+
+                  newRandVal = true;
+                }
+
+              } // end for loop 'j' 
+
+            } // end for loop 'i'
+
+        } // end for loop 'k'
+
+        if (newRandVal) { getRandCel() }
+
+        break;
+      case 38: // Up
+
+        var newRandVal = false;
+
+        for(var i = 1; i <= 4; i++) {
+
+          var columns = $("[data-col=" + i + "]");
+
+          for(var j = 0; j < 4; j++) {
+
+            var current = columns.eq(j);
+
+            for(var k = j + 1; k <= 4; k++) {
+
+              var next = columns.eq(k),
                   nextValue = next.getDataValue(),
                   currentValue = current.getDataValue();
 
@@ -112,7 +185,7 @@ $(function() {
                 current.text(nextValue);
 
                 next.setDataValue(0);
-                next.text(0);
+                next.text("");
 
                 newRandVal = true;
               } else if ((currentValue == nextValue) && (currentValue > 0)) {
@@ -122,25 +195,75 @@ $(function() {
                 current.text(addResult);
 
                 next.setDataValue(0);
-                next.text(0);
+                next.text("");
+
+                updateBoxScore(addResult);
 
                 newRandVal = true;
               }
 
-            } // end for loop 'j' 
+            } // end for loop 'k' 
 
-          } // end for loop 'i'
+          } // end for loop 'j'
 
-        } // end for loop 'k'
+        } // end for loop 'i'
 
         if (newRandVal) { getRandCel() }
 
         break;
-      case 38:
-        // Up
-        break;
-      case 40:
-        // Down
+      case 40: // Down
+
+        var newRandVal = false;
+
+        for(var i = 1; i <= 4; i++) {
+
+          var columns = $("[data-col=" + i + "]");
+
+          console.log(columns);
+
+          for(var j = 3; j >= 0; j--) {
+
+            var current = columns.eq(j);
+
+            for(var k = j - 1; k >= 0; k--) {
+
+              var next = columns.eq(k),
+                  nextValue = next.getDataValue(),
+                  currentValue = current.getDataValue();
+
+              if ((currentValue != 0) && (currentValue < nextValue)) {
+                break;
+              } else if ((currentValue == 0) && (nextValue > 0)) {
+
+                current.setDataValue(nextValue);
+                current.text(nextValue);
+
+                next.setDataValue(0);
+                next.text("");
+
+                newRandVal = true;
+              } else if ((currentValue == nextValue) && (currentValue > 0)) {
+                var addResult = currentValue + nextValue;
+
+                current.setDataValue(addResult);
+                current.text(addResult);
+
+                next.setDataValue(0);
+                next.text("");
+
+                updateBoxScore(addResult);
+
+                newRandVal = true;
+              }
+
+            } // end for loop 'k' 
+
+          } // end for loop 'j'
+
+        } // end for loop 'i'
+
+        if (newRandVal) { getRandCel() }
+        
         break;
       default:
         break;
