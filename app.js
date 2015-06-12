50dd10afff2a2b59fbc57ab1fffc85c00227b92e
Amrta Kautsar Faruqi <amrta.faruqi@gmail.com>
2015-06-12 19:58:57 +0800

    Cleaning up some code

--- app.js
+++ app.js
@@ -19,41 +19,63 @@ $(window).on("resize", function() {
 $(function() {
 
   // init window resize to match column height & width value
+
   $(window).resize();
 
-  // init score
+  // init new game button
+
+  $("button").on("click", function() {
+    location.reload();
+  });
+
+  // init best score
+
+  var bestScore = localStorage.getItem("bestScore");
+
+  if (bestScore) {
+    $("[data-best-score]").text(bestScore);
+  }
+
+  // init generate random cell
 
   function getRandCel(num) {
+
     setTimeout(function() {
       var emptyCells = $("[data-value=0]"),
           emptyCellsCount = emptyCells.size();
 
-      // console.log("emptyCells : ", emptyCellsCount);
-
       if (num == undefined) { num = 1; } // set default value if num is undefined
 
-      // if (emptyCellsCount > 0) {
-        for(var i = 0; i < num; i++) {
+        for(var y = 0; y < num; y++) {
 
           var randIndex = _.random(0, emptyCellsCount - 1),
-              selectedCell = emptyCells.eq(randIndex);
+              selectedCell = emptyCells.eq(randIndex),
+              randVal = _.sample([2,4]);
 
-          selectedCell.setDataValue(2);
-          selectedCell.text(2);
+          selectedCell.setDataValue(randVal);
+          selectedCell.text(randVal);
 
           emptyCells = $("[data-value=0]");
           emptyCellsCount = emptyCells.size();
 
           if (emptyCellsCount == 0) {
-            console.log("check available move");
             
             if (!checkAvailableMove()) {
+              var score = parseInt($("[data-score]").text());
+
+              if ( score > parseInt(localStorage.getItem("bestScore")) ) {
+                localStorage.setItem("bestScore", score);  
+                $("[data-best-score]").text(score);
+              }
+
               alert("Game Over");
             }
+
           }
 
-        }
+        } // end of for loop
     }, 500);
+
   }
 
   function checkAvailableMove() {
@@ -62,8 +84,6 @@ $(function() {
   }
 
   function checkHorizontalMove() {
-
-    console.log("checkHorizontalMove");
 
     var foundMove = false;
 
@@ -89,8 +109,6 @@ $(function() {
 
   function checkVerticalMove() {
 
-    console.log("checkVerticalMove");
-
     var foundMove = false;
 
     for(var x = 1; x <= 4; x++) {
@@ -113,6 +131,11 @@ $(function() {
     return foundMove;
   }
 
+  function checkResult(result) {
+    if (result == 2048) {
+      window.open("https://youtu.be/3GwjfUFyY6M?t=18s");
+    }
+  }
 
   function initGrid() {
     getRandCel(2);
@@ -127,214 +150,235 @@ $(function() {
     return;
   }
 
+  function moveLeft() {
+    var newRandVal = false;
+
+    for(var x = 1; x <= 4; x++) {
+
+      var currentRow = $("[data-row=" + x + "]");
+
+        for(var y = 1; y <= 4; y++) {
+          var current = currentRow.find("[data-col=" + y + "]");
+
+          for(var z = y + 1; z <= 4; z++) {
+            var next = currentRow.find("[data-col=" + z + "]"),
+                nextValue = next.getDataValue(),
+                currentValue = current.getDataValue();
+
+            if ((currentValue != 0) && (currentValue < nextValue)) {
+              break;
+            } else if ((currentValue == 0) && (nextValue > 0)) {
+
+              current.setDataValue(nextValue);
+              current.text(nextValue);
+
+              next.setDataValue(0);
+              next.text("");
+
+              newRandVal = true;
+            } else if ((currentValue == nextValue) && (currentValue > 0)) {
+              var addResult = currentValue + nextValue;
+
+              current.setDataValue(addResult);
+              current.text(addResult);
+
+              next.setDataValue(0);
+              next.text("");
+
+              updateBoxScore(addResult);
+              checkResult(addResult);
+
+              newRandVal = true;
+            }
+
+          } // end for loop 'z' 
+
+        } // end for loop 'y'
+
+    } // end for loop 'x'
+
+    if (newRandVal) { getRandCel(); }
+
+    return;
+  }
+
+  function moveRight() {
+    var newRandVal = false;
+    
+    for(var x = 1; x <= 4; x++) {
+
+      var currentRow = $("[data-row=" + x + "]");
+
+        for(var y = 4; y >= 1; y--) {
+          var current = currentRow.find("[data-col=" + y + "]");
+
+          for(var z = y - 1; z >= 1; z--) {
+            var next = currentRow.find("[data-col=" + z + "]"),
+                nextValue = next.getDataValue(),
+                currentValue = current.getDataValue();
+
+            if ((currentValue != 0) && (currentValue < nextValue)) {
+              break;
+            } else if ((currentValue == 0) && (nextValue > 0)) {
+
+              current.setDataValue(nextValue);
+              current.text(nextValue);
+
+              next.setDataValue(0);
+              next.text("");
+
+              newRandVal = true;
+            } else if ((currentValue == nextValue) && (currentValue > 0)) {
+              var addResult = currentValue + nextValue;
+
+              current.setDataValue(addResult);
+              current.text(addResult);
+
+              next.setDataValue(0);
+              next.text("");
+
+              updateBoxScore(addResult);
+              checkResult(addResult);
+
+              newRandVal = true;
+            }
+
+          } // end for loop 'z' 
+
+        } // end for loop 'y'
+
+    } // end for loop 'x'
+
+    if (newRandVal) { getRandCel(); }
+
+    return;
+  }
+
+  function moveUp() {
+    var newRandVal = false;
+
+    for(var y = 1; y <= 4; y++) {
+
+      var columns = $("[data-col=" + y + "]");
+
+      for(var z = 0; z < 4; z++) {
+
+        var current = columns.eq(z);
+
+        for(var x = z + 1; x <= 4; x++) {
+
+          var next = columns.eq(x),
+              nextValue = next.getDataValue(),
+              currentValue = current.getDataValue();
+
+          if ((currentValue != 0) && (currentValue < nextValue)) {
+            break;
+          } else if ((currentValue == 0) && (nextValue > 0)) {
+
+            current.setDataValue(nextValue);
+            current.text(nextValue);
+
+            next.setDataValue(0);
+            next.text("");
+
+            newRandVal = true;
+          } else if ((currentValue == nextValue) && (currentValue > 0)) {
+            var addResult = currentValue + nextValue;
+
+            current.setDataValue(addResult);
+            current.text(addResult);
+
+            next.setDataValue(0);
+            next.text("");
+
+            updateBoxScore(addResult);
+            checkResult(addResult);
+
+            newRandVal = true;
+          }
+
+        } // end for loop 'x' 
+
+      } // end for loop 'z'
+
+    } // end for loop 'y'
+
+    if (newRandVal) { getRandCel(); }
+
+    return;
+  }
+
+  function moveDown() {
+    var newRandVal = false;
+
+    for(var y = 1; y <= 4; y++) {
+
+      var columns = $("[data-col=" + y + "]");
+
+      for(var z = 3; z >= 0; z--) {
+
+        var current = columns.eq(z);
+
+        for(var x = z - 1; x >= 0; x--) {
+
+          var next = columns.eq(x),
+              nextValue = next.getDataValue(),
+              currentValue = current.getDataValue();
+
+          if ((currentValue != 0) && (currentValue < nextValue)) {
+            break;
+          } else if ((currentValue == 0) && (nextValue > 0)) {
+
+            current.setDataValue(nextValue);
+            current.text(nextValue);
+
+            next.setDataValue(0);
+            next.text("");
+
+            newRandVal = true;
+          } else if ((currentValue == nextValue) && (currentValue > 0)) {
+            var addResult = currentValue + nextValue;
+
+            current.setDataValue(addResult);
+            current.text(addResult);
+
+            next.setDataValue(0);
+            next.text("");
+
+            updateBoxScore(addResult);
+            checkResult(addResult);
+
+            newRandVal = true;
+          }
+
+        } // end for loop 'x' 
+
+      } // end for loop 'z'
+
+    } // end for loop 'y'
+
+    if (newRandVal) { getRandCel(); }
+
+    return;
+  }
+
   function checkDirection(direction) {
     switch (direction) {
       case 37: // Left 
         
-        var newRandVal = false;
-
-        for(var k = 1; k <= 4; k++) {
-
-          var currentRow = $("[data-row=" + k + "]");
-
-            for(var i = 1; i <= 4; i++) {
-              var current = currentRow.find("[data-col=" + i + "]");
-
-              for(var j = i + 1; j <= 4; j++) {
-                var next = currentRow.find("[data-col=" + j + "]"),
-                    nextValue = next.getDataValue(),
-                    currentValue = current.getDataValue();
-
-
-                if ((currentValue != 0) && (currentValue < nextValue)) {
-                  break;
-                } else if ((currentValue == 0) && (nextValue > 0)) {
-
-                  current.setDataValue(nextValue);
-                  current.text(nextValue);
-
-                  next.setDataValue(0);
-                  next.text("");
-
-                  newRandVal = true;
-                } else if ((currentValue == nextValue) && (currentValue > 0)) {
-                  var addResult = currentValue + nextValue;
-
-                  current.setDataValue(addResult);
-                  current.text(addResult);
-
-                  next.setDataValue(0);
-                  next.text("");
-
-                  updateBoxScore(addResult);
-
-                  newRandVal = true;
-                }
-
-              } // end for loop 'j' 
-
-            } // end for loop 'i'
-
-        } // end for loop 'k'
-
-        if (newRandVal) { getRandCel() }
-
+        moveLeft();
         break;
       case 39: // Right
 
-        var newRandVal = false;
-        
-        for(var k = 1; k <= 4; k++) {
-
-          var currentRow = $("[data-row=" + k + "]");
-
-            for(var i = 4; i >= 1; i--) {
-              var current = currentRow.find("[data-col=" + i + "]");
-
-              for(var j = i - 1; j >= 1; j--) {
-                var next = currentRow.find("[data-col=" + j + "]"),
-                    nextValue = next.getDataValue(),
-                    currentValue = current.getDataValue();
-
-                if ((currentValue != 0) && (currentValue < nextValue)) {
-                  break;
-                } else if ((currentValue == 0) && (nextValue > 0)) {
-
-                  current.setDataValue(nextValue);
-                  current.text(nextValue);
-
-                  next.setDataValue(0);
-                  next.text("");
-
-                  newRandVal = true;
-                } else if ((currentValue == nextValue) && (currentValue > 0)) {
-                  var addResult = currentValue + nextValue;
-
-                  current.setDataValue(addResult);
-                  current.text(addResult);
-
-                  next.setDataValue(0);
-                  next.text("");
-
-                  updateBoxScore(addResult);
-
-                  newRandVal = true;
-                }
-
-              } // end for loop 'j' 
-
-            } // end for loop 'i'
-
-        } // end for loop 'k'
-
-        if (newRandVal) { getRandCel() }
-
+        moveRight();
         break;
       case 38: // Up
 
-        var newRandVal = false;
-
-        for(var i = 1; i <= 4; i++) {
-
-          var columns = $("[data-col=" + i + "]");
-
-          for(var j = 0; j < 4; j++) {
-
-            var current = columns.eq(j);
-
-            for(var k = j + 1; k <= 4; k++) {
-
-              var next = columns.eq(k),
-                  nextValue = next.getDataValue(),
-                  currentValue = current.getDataValue();
-
-              if ((currentValue != 0) && (currentValue < nextValue)) {
-                break;
-              } else if ((currentValue == 0) && (nextValue > 0)) {
-
-                current.setDataValue(nextValue);
-                current.text(nextValue);
-
-                next.setDataValue(0);
-                next.text("");
-
-                newRandVal = true;
-              } else if ((currentValue == nextValue) && (currentValue > 0)) {
-                var addResult = currentValue + nextValue;
-
-                current.setDataValue(addResult);
-                current.text(addResult);
-
-                next.setDataValue(0);
-                next.text("");
-
-                updateBoxScore(addResult);
-
-                newRandVal = true;
-              }
-
-            } // end for loop 'k' 
-
-          } // end for loop 'j'
-
-        } // end for loop 'i'
-
-        if (newRandVal) { getRandCel() }
-
+        moveUp();
         break;
       case 40: // Down
 
-        var newRandVal = false;
-
-        for(var i = 1; i <= 4; i++) {
-
-          var columns = $("[data-col=" + i + "]");
-
-          console.log(columns);
-
-          for(var j = 3; j >= 0; j--) {
-
-            var current = columns.eq(j);
-
-            for(var k = j - 1; k >= 0; k--) {
-
-              var next = columns.eq(k),
-                  nextValue = next.getDataValue(),
-                  currentValue = current.getDataValue();
-
-              if ((currentValue != 0) && (currentValue < nextValue)) {
-                break;
-              } else if ((currentValue == 0) && (nextValue > 0)) {
-
-                current.setDataValue(nextValue);
-                current.text(nextValue);
-
-                next.setDataValue(0);
-                next.text("");
-
-                newRandVal = true;
-              } else if ((currentValue == nextValue) && (currentValue > 0)) {
-                var addResult = currentValue + nextValue;
-
-                current.setDataValue(addResult);
-                current.text(addResult);
-
-                next.setDataValue(0);
-                next.text("");
-
-                updateBoxScore(addResult);
-
-                newRandVal = true;
-              }
-
-            } // end for loop 'k' 
-
-          } // end for loop 'j'
-
-        } // end for loop 'i'
-
-        if (newRandVal) { getRandCel() }
-        
+        moveDown();
         break;
       default:
         break;
